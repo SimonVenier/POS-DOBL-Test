91f65d5eb574bd025761d96be41777b149870ad2
Simon <simvenier@hotmail.com>
2020-11-18 17:19:53 +0100

    Hab was vergessen

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,37 @@
 package com.example.demo;
 
+import models.Kunde;
+import models.Rechnung;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reps.KundeInter;
+import reps.RechnungInter;
 
 @SpringBootApplication
-public class DemoApplication {
-
-    public static void main(String[] args) {
+public class DemoApplication
+{
+    public static void main(String[] args)
+    {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    public CommandLineRunner ausgaben(KundeInter kundeInter, RechnungInter rechnungInter)
+    {
+        return (args) ->
+        {
+            Kunde k1 = new Kunde();
+            k1.setVorname("Simon");
+            System.out.println(kundeInter);
+
+            Rechnung r1 = new Rechnung();
+            r1.getRechnungsbetrag();
+            System.out.println(rechnungInter);
+
+        };
+
+    }
+
 }
+
+
