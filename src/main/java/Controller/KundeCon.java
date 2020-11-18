b2e71cfe6e26a3b1e8d2a5640c8625fbe42e205b
Simon <simvenier@hotmail.com>
2020-11-18 15:42:40 +0100

    Fertig

--- src/main/java/Controller/KundeCon.java
+++ src/main/java/Controller/KundeCon.java
@@ -1,11 +1,40 @@
 package Controller;
 
+import models.Kunde;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import reps.KundeInter;
 import reps.RechnungInter;
+import service.KundeService;
+import service.RechnungSevice;
 
+import java.util.List;
 public class KundeCon
 {
     KundeInter kundeInter;
     RechnungInter rechnungInter;
+    KundeService kundeService;
+    RechnungSevice rechnungSevice;
+
+    public KundeCon(KundeInter kundeInter, RechnungInter rechnungInter, KundeService kundeService, RechnungSevice rechnungSevice) {
+        this.kundeInter = kundeInter;
+        this.rechnungInter = rechnungInter;
+        this.kundeService = kundeService;
+        this.rechnungSevice = rechnungSevice;
+    }
+
+    @GetMapping("/kunden")
+    public ResponseEntity<List<Kunde>> alleKunden()
+    {
+        return ResponseEntity.ok(kundeService.alleKunden());
+    }
+
+    @PostMapping("/kunde")
+    public ResponseEntity<Kunde> kundeHizufuegen(@RequestBody Kunde kunde)
+    {
+        return ResponseEntity.ok(kundeService.kundeHinzufuegen(kunde));
+    }
 
 }
--- src/main/java/Controller/RechnungCon.java
+++ src/main/java/Controller/RechnungCon.java
@@ -1,4 +1,32 @@
 package Controller;
 
-public class RechnungCon {
+import models.Kunde;
+import models.Rechnung;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import service.RechnungSevice;
+
+import java.util.List;
+
+public class RechnungCon
+{
+    private RechnungSevice rechnungSevice;
+
+    public RechnungCon(RechnungSevice rechnungSevice) {
+        this.rechnungSevice = rechnungSevice;
+    }
+
+    @GetMapping("/rechnungen")
+    public ResponseEntity<List<Rechnung>> alleRechnungen()
+    {
+        return ResponseEntity.ok(rechnungSevice.alleRechnungen());
+    }
+
+    @PostMapping("/rechnung")
+    public ResponseEntity<Rechnung> rechnungHizufuegen(@RequestBody Rechnung rechnung)
+    {
+        return ResponseEntity.ok(rechnungSevice.rechnungHinzufuegen(rechnung));
+    }
 }
